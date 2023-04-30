e9649cc0ee9b5b6a78e8a61b9bcb3d6b063f3cd5
Sam <isco.easa123@gmail.com>
2023-04-30 09:38:05 +0530

    MyTeenis Club Is Here,Join!

---
+++ members/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . models import Member 
+
+# Register your models here.
+
+class MemberAdmin(admin.ModelAdmin) :
+    list_display = ("firstname","lastname","joined_date",)
+    prepopulated_fields = {"slug": ("firstname", "lastname")}
+
+admin.site.register(Member,MemberAdmin) 
---
+++ members/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from django.template import loader 
+from .models import Member
+# Create your views here.
+
+def members(request) : 
+    mymembers = Member.objects.all().values() 
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers' : mymembers,
+    }
+    return HttpResponse (template.render(context,request))
+
+def details (request, slug) : 
+    mymember =Member.objects.get(slug=slug)  
+    template = loader.get_template('details.html')
+    context= {
+        'mymember' : mymember,
+    }
+    return HttpResponse(template.render(context, request)) 
+
+
+def main(request) : 
+    template = loader.get_template('main.html')
+    return HttpResponse (template.render()) 
+    
+
+
